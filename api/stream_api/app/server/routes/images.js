1b5b7f9e527f5086cf3bc939cb43d124c86080d1
Remousses <remy91490@hotmail.fr>
2021-03-21 15:50:42 +0100

    improved image search

--- api/stream_api/app/server/routes/images.js
+++ api/stream_api/app/server/routes/images.js
@@ -1,8 +1,11 @@
 const routes = require('express').Router(),
-    { check } = require('express-validator');
+    { check } = require('express-validator'),
+    fs = require('fs'),
+    path = require('path'),
+    btoa = require('btoa')
+    Util = require('util');
 
-const searchImage = require('../common/searchImage'),
-    errorFile = require('../common/error');
+const errorFile = require('../common/error');
 
 routes.get('/searchImage', [
     check('path').not().isEmpty().withMessage(errorFile.commonErrorMessage)
@@ -13,5 +16,18 @@ routes.get('/searchImage', [
 
     searchImage(req, res, repo);
 });
+
+function searchImage(req, res, repo) {
+    const error = errorFile.checkError(req);
+    if (error) {
+        return res.status(422).json(error);
+    }
+
+    const bufferImage = fs.readFileSync(path.resolve(__dirname.replace('/app/server/routes', '/app'), repo));
+
+    return res.status(200).send({
+        image: 'data:image/' + repo.split('.')[1] + ';base64,' + new Buffer(bufferImage).toString('base64')
+    });
+}
 
 module.exports = routes;
