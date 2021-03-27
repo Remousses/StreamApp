272f939a5d740aca33b9403660f7aacfce33db4a
Remousses <remy91490@hotmail.fr>
2021-03-28 00:00:59 +0100

    upload lonks OK (back only)

--- api/stream_api/app/server/routes/uploads.js
+++ api/stream_api/app/server/routes/uploads.js
@@ -3,7 +3,7 @@ const routes = require('express').Router(),
     fs = require('fs'),
     { check } = require('express-validator'),
     rimraf = require("rimraf"),
-    shell = require('shelljs');
+    request = require('request');
 
 const storage = multer.diskStorage({
     destination: (req, file, callBack) => {
@@ -18,43 +18,116 @@ const upload = multer({ storage: storage });
 
 const errorFile = require('../common/error');
 
-routes.put('/uploads/files', upload.array('files'),
-    (req, res, next) => {
-        const files = req.files;
+routes.put('/uploads/files', upload.array('files'), (req, res, next) => {
+    const files = req.files;
 
-        if (!files) {
-            const error = new Error('No File')
-            error.httpStatusCode = 400
-            return next(error)
-        }
-        console.log('Télechargement de tous les fichiers terminé');
-        
-        res.send({ filesOploaded: true }).end();
+    if (!files) {
+        const error = new Error('No File')
+        error.httpStatusCode = 400
+        return next(error)
     }
-);
-
+    console.log('Télechargement de tous les fichiers terminé');
+    
+    return res.json({ filesOploaded: true });
+});
 
 routes.put('/upload/links', [
     check('folderDestination').not().isEmpty().withMessage(errorFile.commonErrorMessage),
     check('links').not().isEmpty().withMessage(errorFile.commonErrorMessage)
-], (req, res) => {
-    let error = errorFile.checkError(req);
-    if (error) {
-        return res.status(422).json(error);
-    }
+], async (req, res) => {
+    errorFile.checkError(req, res);
 
-    let folderDestination = req.query.folderDestination;
-    let links = req.query.links;
+    const body = req.body;
+    const folderDestination = body.folderDestination + '/';
+    const links = body.links;
 
-    let test = shell.exec('./server/bash-scripts/upload-links.sh ' + folderDestination + ' ' + links);
+    const errors = [];
+    const allLinks = await checkUploadLinksErrors(folderDestination, links, errors);
     
-    console.log();
-    console.log("output", test.stdout)
-    
-    res.status(200).send({
-        res: 'OK'
-    }).end();
+    return errors.length != 0
+        ? res.status(400).json({ err: errors })
+        : res.status(200).json({ res: allLinks });
 });
+
+function checkUploadLinksErrors(folderDestination, links, errors) {
+    // return new Promise((resolve, reject) => {
+        const allLinks = links.map(link => {
+            return uploadLink(folderDestination, link)
+        })
+        return Promise.all(allLinks).then(res => {
+            console.log("Result of many link upload : ", res);
+
+            res.forEach(file => {
+                // console.log("aadeadazdazdzad", file)
+                // console.log("ezzfregse<fwrgdrgfqer", file.err)
+                if (file.err) {
+                    errors.push({ err: file.err });
+                } else {
+                    console.log("yolo", file);
+                }
+            })
+            // return res;
+        });
+    // });
+}
+
+function uploadLink(folderDestination, link) {
+    return new Promise((resolve, reject) => {
+        const fileName = link.substr(link.lastIndexOf('/') + 1);
+        const path = folderDestination + fileName;
+        const linkError = { fileName, error : {}};
+
+        console.log('Création du fichier ' + fileName + ' dans le dossier ' + folderDestination)
+
+        const file = fs.createWriteStream(path);
+
+        // on lance le téléchargement
+        const sendReq = request.get(link);
+
+        // on vérifie la validité du code de réponse HTTP
+        sendReq.on('response', (response) => {
+            if (response.statusCode !== 200) {
+                console.log('Response status was ' + response.statusCode);
+                error = true;
+                linkError.error.statusCode = statusCode;
+                reject({ err : linkError });
+            }
+        });
+
+        // au cas où request rencontre une erreur on efface le fichier partiellement écrit
+        // puis on passe l'erreur au callback
+        sendReq.on('error', (err) => {
+            console.log('Une erreur c\'est produit, durant la récupération du lien', link, err.message);
+            fs.unlink(path);
+            
+            linkError.error.request = err.message;
+            reject({ err : linkError });
+        });
+
+        // écrit directement le fichier téléchargé
+        sendReq.pipe(file);
+
+        // lorsque le téléchargement est terminé on appelle le callback
+        file.on('finish', () => {
+            // close étant asynchrone, le cb est appelé lorsque close a terminé
+            file.close(_ => console.log('File ' + fileName +  ' was successfully closed'));
+            resolve({ res: 'File ' + fileName + ' was successfully uploaded'})
+        });
+
+        // si on rencontre une erreur lors de l'écriture du fichier
+        // on efface le fichier puis on passe l'erreur au callback
+        file.on('error', (err) => {
+            // on efface le fichier sans attendre son effacement
+            // on ne vérifie pas non plus les erreur pour l'effacement
+            console.log('Une erreur c\'est produit, suppression du fichier', err.message);
+            fs.unlink(path);
+            linkError.error.write = err.message;
+            reject({ err : linkError });
+        });
+    }).catch(err => {
+        return err;
+    });
+}
 
 routes.put('/createFolder', (req, res) => {
     const body = req.body;
@@ -71,28 +144,26 @@ routes.put('/createFolder', (req, res) => {
         return res.status(200).json({ folderCreated: true });
     }
     
-    res.status(404).send({ folderCreated: false }).end();
+    return res.status(404).json({ folderCreated: false });
 });
 
 routes.delete('/deleteFile',[
     check('currentFolder').not().isEmpty().withMessage(errorFile.commonErrorMessage),
     check('fileName').not().isEmpty().withMessage(errorFile.commonErrorMessage)
 ], (req, res) => {
-    let query = req.query;
-    let path = query.currentFolder + '/' + query.fileName;
+    const query = req.query;
+    const path = query.currentFolder + '/' + query.fileName;
 
     console.log('Suppression du fichier ' + path + ' en cours');
     
     
-    // fs.close(16)
-
     fs.unlink(path, (err) => {
         if (err) {
             console.log('Erreur lors de la suppression du fichier ' + path, err);
-            res.sendStatus(500);
+            return res.sendStatus(500);
         } else {
             console.log('Suppression du fichier ' + path + ' terminé');
-            res.status(200).send({ fileCreated: true }).end();
+            return res.status(200).json({ fileCreated: true });
         }
     });
 });
@@ -100,13 +171,13 @@ routes.delete('/deleteFile',[
 routes.delete('/deleteFolder',[
     check('currentFolder').not().isEmpty().withMessage(errorFile.commonErrorMessage),
 ], (req, res) => {
-    let currentFolder = req.query.currentFolder;
+    const currentFolder = req.query.currentFolder;
 
     console.log('Suppression du dossier ' + currentFolder + ' en cours');
     
     rimraf(currentFolder, () => {
         console.log('Suppression du dossier ' + currentFolder + ' terminé')
-        res.status(200).send({ folderDeleted: true })
+        res.status(200).json({ folderDeleted: true }).end()
     });
 });
 
